c89ee8e8203af83ecddba9056467d85aef9129b7
Hojin Koh <hojin-koh@users.noreply.github.com>
2022-06-26 09:57:25 +0800

    Delete trailing spaces

--- Eikthyr/data.py
+++ Eikthyr/data.py
@@ -1,11 +1,11 @@
 # Copyright 2021-2022, Hojin Koh
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -98,5 +98,5 @@ class Target(lg.LocalTarget):
         # Check hashes of dependencies
         if self.task.getSrcHash() != objGen['src']:
             return True
-                
+               
         return False
