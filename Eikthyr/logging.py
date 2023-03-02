af6a78f305b077e75264126e07b87861f0a0ac6d
Hojin Koh <hojin-koh@users.noreply.github.com>
2023-03-02 16:34:51 +0800

    Bump date in source code header

--- Eikthyr/logging.py
+++ Eikthyr/logging.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2021-2022, Hojin Koh
+# Copyright 2021-2023, Hojin Koh
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
